2ec42d8677aa3619e0e6d16022abc7102eb4e846
unknown <divyansh8252230@gmail.com>
2021-06-21 20:29:24 +0530

    Home Updates

---
+++ js/navbar.js
@@ -0,0 +1,11 @@
+function myFunction(x) {
+    console.log(x);
+    x.classList.toggle("change");
+
+    let menu = document.querySelector('.links');
+
+    menu.classList.toggle("toggle");
+
+
+  }
+

3da8b2132e9abeec4c73fdf7a3600d0b09dc0d4d
unknown <divyansh8252230@gmail.com>
2021-06-23 21:06:23 +0530

    Updates

--- js/home.js
+++ js/home.js
@@ -25,7 +25,7 @@ window.addEventListener('resize',()=>{
 
 
 
-function myFunction(x) {
+function aFunction(x) {
     
     console.log(x);
     x.classList.toggle("change");
@@ -61,16 +61,16 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  // var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
+  // for (i = 0; i < dots.length; i++) {
+  //     dots[i].className = dots[i].className.replace(" active", "");
+  // }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  // dots[slideIndex-1].className += " active";
 }
  
--- js/index.js
+++ js/index.js
@@ -1,24 +1,154 @@
 data = [
   {
-    head: 'Dream 2',
-    para: 'Thriller,Mystery & Comedy',
-    img: './Images/Image1.jfif',
-    type : 'Mystery'
+    head: 'Haider',
+    para: 'Action, Crime, Drama',
+    img: '../Images/Drama/Haider.jpg',
+    type : 'Drama'
 
   },
   {
     head: 'Batman',
     para: 'A Marvel Comics superhero team.',
-    img: './Images/Image1.jfif',
+    img: '../Images/Action/batman.jpg',
     type: 'Action'
   },
   {
     head: 'The Misfits',
-    para: ' Action, Adventure, Thriller',
+    para: 'Action, Adventure, Thriller',
     img: './Images/The Misfits.jpg',
-    type : 'Mystery'
-  }
+    type : 'Drama'
+  },
+  {
+    head: 'Gravity',
+    para: 'Mystery & Thriller, Sci-Fi, Drama',
+    img: './Images/Sci-Fi/Gravity.jpg',
+    type : 'Sci-Fi'
+  },
+  {
+    head: 'Black Panther',
+    para: 'Adventure, Fantasy, Action',
+    img: './Images/Action/black-panther.jpg',
+    type : 'Action'
+  },
+  {
+    head: 'Iron Man',
+    para: 'Sci-Fi, Adventure, Fantasy, Action',
+    img: './Images/Sci-Fi/Iron Man.jpg',
+    type : 'Sci-Fi'
+  },
+  {
+    head: 'Knives Out',
+    para: 'Crime, Comedy, Mystery & Thriller, Drama',
+    img: './Images/Drama/Knives-out.jpg',
+    type : 'Drama'
+  },
+  {
+    head: 'Mad Max',
+    para: 'Adventure, Fantasy, Action',
+    img: './Images/Action/mad-max.jpg',
+    type : 'Action'
+  },
+  {
+    head: 'Mission Impossible',
+    para: 'Adventure, Mystery & Thriller, Action',
+    img: './Images/Action/mission-impossible.jpg',
+    type : 'Action'
+  },
+  {
+    head: 'The Martian',
+    para: 'Drama, Sci-Fi, Adventure',
+    img: './Images/Sci-Fi/The Martian.jpg',
+    type : 'Sci-Fi'
+  },
+  {
+    head: 'X-Men Dark Phoenix',
+    para: 'Adventure, Fantasy, Sci-Fi, Action',
+    img: './Images/Sci-Fi/X-men Dark Phoenix.jpg',
+    type : 'Sci-Fi'
+  },
+  {
+    head: 'La La Land',
+    para: 'Romance, Comedy, Musical, Drama',
+    img: './Images/Drama/la-la-land.jpg',
+    type : 'Drama'
+  },
+  {
+    head: 'Creed(2015)',
+    para: 'Drama, Action',
+    img: './Images/Drama/Creed.jpg',
+    type : 'Drama'
+  },
+  {
+    head: 'Sholay (1975)',
+    para: 'Action, Crime, Drama',
+    img: './Images/Drama/Sholay.jpg',
+    type : 'Drama'
+  },
+  {
+    head: 'Spider Man',
+    para: 'Adventure, Fantasy, Action',
+    img: './Images/Action/spider-man.jpg',
+    type : 'Action'
+  },
+  {
+    head: 'Thor',
+    para: 'Action, Fantasy, Comedy, Sci-Fi, Adventure',
+    img: './Images/Action/thor.jpg',
+    type : 'Action'
+  },
+  {
+    head: 'Wonder Woman',
+    para: 'Adventure, Fantasy, Action',
+    img: './Images/Action/wonder-woman.jpg',
+    type : 'Action'
+  },
+  {
+    head: 'Baahubali 2: The Conclusion',
+    para: 'Action, Drama',
+    img: './Images/Drama/Bahubali.jpg',
+    type : 'Drama'
+  },
+  {
+    head: 'Looper',
+    para: 'Sci-Fi, Mystery & Thriller, Action',
+    img: './Images/Sci-Fi/looper.jpg',
+    type : 'Sci-Fi'
+  },
+  {
+    head: 'Captain Marvel',
+    para: 'Sci-Fi, Adventure, Fantasy, Action',
+    img: './Images/Sci-Fi/captain_marvel.jpg',
+    type : 'Sci-Fi'
+  },
+  {
+    head: 'Hulk',
+    para: 'Adventure, Fantasy, Action',
+    img: './Images/Action/Hulk.jpg',
+    type : 'Action'
+  },
+  {
+    head: '007',
+    para: 'Crime, Drama, Action',
+    img: './Images/Drama/007.jpg',
+    type : 'Drama'
+  },
+  {
+    head: 'Big Hero 6',
+    para: 'Sci-Fi, Adventure, Action, Fantasy',
+    img: './Images/Sci-Fi/big-hero.jpg',
+    type : 'Sci-Fi'
+  },
+  {
+    head: 'Captain America',
+    para: 'Sci-Fi, Adventure, Action, Fantasy',
+    img: './Images/Sci-Fi/captain-america.jpg',
+    type : 'Sci-Fi'
+  },
 ]
+
+
+
+
 
 
 
@@ -30,7 +160,7 @@ for (let i = 0; i < data.length; i++) {
   var cln = card.cloneNode(card)
   cln.className = "clone";
   cln.classList.add(data[i].type);
-  document.getElementById("container").appendChild(cln)
+  document.getElementById("gallContainer").appendChild(cln)
   let h1 = document.querySelectorAll('.clone .text h2');
   let p = document.querySelectorAll('.clone .text p');
   let img = document.querySelectorAll('.clone img');
